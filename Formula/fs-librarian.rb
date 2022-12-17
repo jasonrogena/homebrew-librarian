0a934f836beef861870322b63adaee654042d12f
Jason Rogena <jason@rogena.me>
2022-12-17 17:59:30 +0000

    Update fs-librarian version to 0.3.0
    Fixes #
    Effort: 1:00 hours
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- Formula/fs-librarian.rb
+++ Formula/fs-librarian.rb
@@ -1,20 +1,19 @@
 class FsLibrarian < Formula
-  desc "Librarian runs pre-configured commands against a group of files that match a set of filters"
+  desc "Runs pre-configured commands against a group of files that match some filters"
   homepage "https://github.com/jasonrogena/librarian"
+  url "https://github.com/jasonrogena/librarian.git",
+    tag:      "v0.3.0",
+    revision: "a67602eda640e1f34e96c76dc06bb7fae028b26d"
   license "MIT"
   head "https://github.com/jasonrogena/librarian.git", branch: "main"
-  url "https://github.com/jasonrogena/librarian.git",
-    tag:      "v0.2.1",
-    revision: "c23105547a5f403143f976e5da2b4a7ddc8108a0"
 
   depends_on "make" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "make", "build"
     bin.install "target/release/fs-librarian"
   end
- 
+
   test do
     system "target/release/fs-librarian", "test", "mime", "LICENSE"
   end
